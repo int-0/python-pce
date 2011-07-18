12a610c4fce72f63f82ac65d2097b1b26f607bfb
tobias.deb@gmail.com <tobias.deb@gmail.com@2ade21f2-caaa-939c-ce81-5ab92ef2f372>
2011-07-18 15:35:12 +0000

    Action skeleton added.

--- trunk/pre-src/actor.py
+++ trunk/pre-src/actor.py
@@ -7,4 +7,37 @@ class Actor(Drawable):
     def __init__(self, initial_anim_stack):
         self.Drawable(self, initial_anim_stack)
 
-    
+        self.state = { 'actions' : [],
+                       'actual_action' : None }
+
+    def do_action(self, action):
+        # FIXME: check if action is valid
+        self.state['actions'].append(action)
+
+    def next_action(self):
+        if len(self.state['actions']) > 0:
+            self.state['actual_action'] = self.state['actions'].pop()
+        else:
+            self.cancel_all_actions()
+
+    def cancel_all_actions(self):
+        self.state['actual_action'] = None
+        # Activate Stand-by animation
+
+    def walk(self):
+        desired_pos = self.state['actual_action']['position']
+
+        # Check if action finished
+        if desired_pos == self.get_pos():
+            self.next_action()
+            return
+
+        # Calculate new direction
+
+        # Select apropiate animation
+
+        # Continue animation
+
+    def say(self):
+        pass
+
